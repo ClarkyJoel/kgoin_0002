25702828fc9f9c4953ddc8b875950213da39ea39
AnhDT <dotheanh@kgoin.com>
2012-09-19 22:02:13 +0700

    uploaded demo source

--- index.php
+++ index.php
@@ -1,24 +1,93 @@
     <!DOCTYPE html>
     <html lang="en">
     <head>
-    <title>Bootstrap Testin..</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CMC Infosec</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/coolMenu.css" rel="stylesheet">
+    <link href="css/js-image-slider.css" rel="stylesheet" type="text/css" />
+    <script src="js/js-image-slider.js"></script>
+	<script src="js/tabs.js"></script>
+	<link href="css/generic.css" rel="stylesheet" type="text/css" />
+    
     <!-- Bootstrap -->
     <link href="css/bootstrap-responsive.css" rel="stylesheet">
     <link href="css/normalize.css" rel="stylesheet">
     </head>
     <body>
-    <h1>Mission Success</h1>
-    <div class="container-fluid">
+		<div class="span11 wrapper">
+			<div class="row">
+			<div id="header">
+				
+					<div class="span3 logo">
+						<?php include "/module/header.php"; ?>
+					</div>
+					<div class="clear"></div>
+					<div class="banner">
+						<img src="images/homepage-banner.png" style="padding-top: 5px; width: 1025px;" />
+					</div>
+				
+			</div>
+			<div id="content">
+					<div class="clear"></div>
+					<h3>Sản Phẩm & Dịch Vụ</h3>
+					<div id="tabContainer">
+					    <div class="tabs">
+						    <ul>
+							    <li id="tabHeader_1">Cá nhân, Gia đình</li>
+							    <li id="tabHeader_2">Doanh Nghiệp</li>
+							    <li id="tabHeader_3">Mobile</li>
+							    <li id="tabHeader_4">Dịch Vụ</li>
+						    </ul>
+					    </div>
+					    <div class="tabscontent">
+					    	<div class="row tabpage" id="tabpage_1">
+					    		<div class="span3 tab" >
+					    			<h3>CMC Antivirus</h3>
+					    			<p>CMC Antivirus là giải pháp giúp bạn bảo vệ máy tính một cách toàn diện và đặc biệt là MIỄN PHÍ cho người dùng cá nhân.</p>
+					    			<a href="cmc-antivirus.php">Xem thêm...</a>
+					    			<img src="images/Antivirus-thumbs-img.png" style="padding-top: 10px" />
+					    		</div>
+					    		<div class="span3 tab">
+					    			<h3>CMC Internet Security</h3>
+					    			<p>CMC Internet Security là giải pháp giúp bạn bảo vệ máy tính một cách toàn diện và đặc biệt là MIỄN PHÍ cho người dùng cá nhân.</p>
+					    			<a href="cmc-internetsecurity.php">Xem thêm...</a>
+					    			<img src="images/InternetSercurity-thumbs-img.png" style="padding-top: 10px" />
+					    		</div>
+					    		<div class="span3 tab">
+					    			<h3>CMC Mobile Security</h3>
+					    			<p>CMC MegaVNN là giải pháp giúp bạn bảo vệ máy tính một cách toàn diện và đặc biệt là MIỄN PHÍ cho người dùng cá nhân.</p>
+					    			<a href="cmc-mobilesecurity.php">Xem thêm...</a>
+					    			<img src="images/MobileSecurity-thumbs.png" style="padding-top: 10px;"/>
+					    		</div>
+					    	</div>
+					    	<div class="row tabpage" id="tabpage_2">
+					    		<h3>DOANH NGHIEP CONTENT...</h3>
+					    	</div>
+					    	<div class="row tabpage" id="tabpage_3">
+					    		<h3>MOBILE CONTENT...</h3>
+					    	</div>
+					    	<div class="row tabpage" id="tabpage_4">
+					    		<h3>DICH VU CONTENT...</h3>
+					    	</div>
+					      
+					    </div>
+					</div>
+					<div id="sidebar">
+						<?php include "/module/news-support-sidebar.php"; ?>
+					</div>
+					<div class="clear"></div>
+					<div id="slider">
+						<?php include "/module/slider.php"?>
+					</div>
+					<div id="embedVideo">
+						<?php include "/module/embedVideo.php"; ?>
+					</div>
+				
+			</div> <!-- .content -->
+			</div>
 			
-			    <div class="span2">
-			    	<h3>CMC Antivirus</h3>
-			    	<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-			    </div>
-			    <div class="span10">
-			    	<h3>CMC Internet Sercurity</h3>
-			    	<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-			    </div>
-    </div>
+		</div> <!-- .Wrapper -->
     </body>
     </html>
---
+++ module/header.php
@@ -0,0 +1,45 @@
+<a href="index.php"><img src="images/cmcinfosec-logo.png" /></a>
+		<div class="span1 text">
+			<h3>CMC InfoSec</h3>
+			<span>An toàn từng click</span>
+		</div>
+	
+</div>
+<div class="span5 search">
+	
+		<span>Diễn Đàn CMC InfoSec</span>
+		<img src="images/search-icon.png">
+		<input name="search" type="text" maxlength="20" class="search-input" />
+		<button class="btn btn-custom" type="button">OK</button>
+	
+</div>
+<div class="clear"></div>
+<div id="mainMenu" class="main-menu">
+	<div class="menu">
+		<ul id="coolMenu" class="main_menu">
+			<li id="menuItem_1" class="active">
+				<a href="index.php">
+					<img src="images/mainMenu-logo.png" class="mainMenuLogo-img" />
+				</a>
+				<ul class="noJS">
+					<li><a href="index.php">Sản Phẩm & Dịch Vụ</a></li>
+					<li><a href="dathang">Đặt Hàng</a></li>
+					<li><a href="download.php">Tải Về</a></li>
+					<li><a href="daily.php">Đại Lý</a></li>
+				</ul>
+			</li>
+			
+			<li id="menuItem_2" ><a href="#">Hỗ Trợ</a></li>
+			<li id="menuItem_3"><a href="#">Liên Hệ</a></li>
+			<li id="menuItem_4">
+				<a href="#">Giới Thiệu</a>
+				<ul class="noJS">
+					<li><a href="#">Hồ Sơ Công Ty</a></li>
+					<li><a href="#">Đối Tác Chiến Lược</a></li>
+					<li><a href="#">Tuyển Dụng</a></li>
+				</ul>
+			</li>
+			<li id="menuItem_5"><a href="#">Tin Tức</a></li>
+		</ul>
+	</div>
+</div>
