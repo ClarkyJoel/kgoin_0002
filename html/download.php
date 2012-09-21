19f0b24cc0d2c61065aca9f942b9388485c51bbe
AnhDT <dotheanh@kgoin.com>
2012-09-21 13:54:36 +0700

    create download page

---
+++ html/download.php
@@ -0,0 +1,67 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <title>CMC Infosec</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/coolMenu.css" rel="stylesheet">
+    <link href="css/js-image-slider.css" rel="stylesheet" type="text/css" />
+    <script src="js/js-image-slider.js"></script>
+	<script src="js/tabs.js"></script>
+	<link href="css/generic.css" rel="stylesheet" type="text/css" />
+    
+    <!-- Bootstrap -->
+    <link href="css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="css/normalize.css" rel="stylesheet">
+    </head>
+    <body>
+    	<div id="wrapper-shadow">
+			<div class="span11 wrapper">
+				<div class="row">
+					<div id="header">
+						<div class="span3 logo">
+							<?php include "/module/header.php"; ?>
+						</div>
+						<div class="clear"></div>
+						<div id="path"><p><a href="index.php">CMC InfoSec.com</a> > Download</p></div>
+						
+					</div>
+					<div class="clear"></div>
+					<div id="content">
+						<div class="downloadContainer">
+							<div class="download-colleft">
+								<div class="menuMoRong">
+									<?php include "module/menuMoRong.php"; ?>
+								</div>
+								<div id="embedVideo_dathang">
+									<?php include "/module/embedVideoDathang.php"; ?>
+								</div>
+							</div>
+							<div class="downloadMaincol">
+								<div class="banner"><img src="images/banner-download.png" /></div>
+							</div>
+							<div class="dathang-rightcol">
+								<div id="sidebar">
+									<?php include "module/news-support-sidebar.php"; ?>
+								</div>
+								<div class="support">
+									<img src="images/hotro_icon.png" />
+								</div>
+								<div id="top10Virus">
+									<?php include "module/top10virus.php"; ?>
+								</div>
+							
+							</div>
+						</div>
+						<div class="clear"></div>
+							<div id="footer">
+							<?php include "module/footer.php"; ?>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+	</html>
